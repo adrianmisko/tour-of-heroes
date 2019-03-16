3c78f52b658a3c3b03e91c4a43c93474906fc25d
adrian <adrian.misko5@gmail.com>
2019-03-16 18:23:36 +0100

    Implement routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HeroesComponent} from './heroes/heroes.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {HeroDetailsComponent} from './hero-details/hero-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'heroes/:id', component: HeroDetailsComponent },
+  { path: 'dashboard', component: DashboardComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
